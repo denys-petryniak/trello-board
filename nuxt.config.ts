ae703bd2341436eefa621ddbb9420e3b6a058bf3
Denys Petryniak <denys.petryniak@gmail.com>
2023-11-24 12:44:43 +0200

    Add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      title: 'Trello Board'
+    }
+  },
   modules: [
     // https://www.npmjs.com/package/@nuxtjs/tailwindcss
     '@nuxtjs/tailwindcss',
